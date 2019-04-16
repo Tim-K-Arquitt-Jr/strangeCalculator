d270cb44f00b8b553902065a749db311f9b3a748
Timothy Arquitt <timothyarquitt@timothys-macbook-pro-2.local>
2019-04-16 17:20:42 -0500

    fully working weird calculator

--- strangeCalculator/ViewController.swift
+++ strangeCalculator/ViewController.swift
@@ -10,10 +10,90 @@ import UIKit
 
 class ViewController: UIViewController {
 
+//    outlets
+    
+    @IBOutlet weak var numberCalDisplay: UILabel!
+    
+    @IBOutlet weak var numDisplaySlide1: UILabel!
+    
+    @IBOutlet weak var numDisplaySlide2: UILabel!
+    
+    @IBOutlet weak var sliderOutlet1: UISlider!
+    
+    @IBOutlet weak var sliderOutlet2: UISlider!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+
+
+    @IBAction func additionPressed(_ sender: Any) {
+     numberCalDisplay.text = String(Int(sliderOutlet1.value+sliderOutlet2.value))
+        
+        
+    }
+    
+    
+    @IBAction func subtractioPressed(_ sender: Any) {
+        
+        numberCalDisplay.text = String(Int(sliderOutlet1.value-sliderOutlet2.value))
+        
+    }
+    
+    
+    
+    @IBAction func multiplicationPressed(_ sender: Any) {
+        numberCalDisplay.text = String(Int(sliderOutlet1.value*sliderOutlet2.value))
+    }
+    
+    
+    
+    @IBAction func divisionPressed(_ sender: Any) {
+        
+        numberCalDisplay.text = String(Int(sliderOutlet1.value/sliderOutlet2.value))
+    }
+    
+    
+    
+    
+    @IBAction func clearPressed(_ sender: Any) {
+        
+        sliderOutlet1.value = 0
+        numDisplaySlide1.text = String(Int(sliderOutlet1.value))
+        
+        
+        sliderOutlet2.value = 0
+        numDisplaySlide2.text = String(Int(sliderOutlet2.value))
+        
+         numberCalDisplay.text = String(Int(sliderOutlet1.value+sliderOutlet2.value))
+        
+    }
+    
+    
+    
+    
+    
+
+    @IBAction func sliderButtonPressed1(_ sender: Any) {
+    
+        numDisplaySlide1.text = String(Int(sliderOutlet1.value))
+    
+    }
+   
+    
+    @IBAction func sliderButtonPressed2(_ sender: Any) {
+    
+        numDisplaySlide2.text = String(Int(sliderOutlet2.value))
+    
+        
+    }
+    
+
+
+
 
 
 }
